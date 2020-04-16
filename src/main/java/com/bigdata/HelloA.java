947e32ec6f14c0c95d68e26c58f3ee6baca43684
yuan_zhigang <3155799044@qq.com>
2020-04-16 18:42:58 +0800

    下班

--- src/main/java/com/bigdata/HelloA.java
+++ src/main/java/com/bigdata/HelloA.java
@@ -3,6 +3,6 @@ package com.bigdata;
 public class HelloA {
 	public static void main(String[] args) {
 		System.out.println("tomhello");
-		System.out.println("hhh");
+		System.out.println("hhhh");
 	}
 }
